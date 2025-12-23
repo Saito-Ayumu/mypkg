319eec020a011b4d12c6f825f4369bfc30b9ce97
Ayumu Saito <saito.ayumu1002@icoud.com>
2025-12-23 16:20:55 +0900

    Add a command

--- setup.py
+++ setup.py
@@ -13,13 +13,10 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.launch.py')),
-    ],
+        ('share/ament_index/resource_index/packages', ['resource/mypkg']),
+        ('share/mypkg', ['package.xml']),
+        ('share/mypkg/launch', glob('launch/*.launch.py')),
+    ]
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='...',
